fdf4cb39774440821f7a4be6e7e7f9c6e099dc80
Noah Bosshart <noahbosshart@gmail.com>
2019-10-23 09:47:39 +0200

    adding new skripts and checkbox for correlated slope

--- Code/ML_Shiny/MA_Shiny/app.R
+++ Code/ML_Shiny/MA_Shiny/app.R
@@ -8,11 +8,16 @@ library(viridis)
 
 ui <- navbarPage(title = "Multilevel Analyse von genesteten Daten", 
 tabPanel(title = "Einführung",
+         tags$head(
+           tags$style(HTML("hr{border-top: 1px solid #95a5a6})")
+                      )
+           ),
          p("Kurze Einführung in die Thematik und vorstellen eines Beispiels z.B. Schüler in Klassen, die eine Prüfung schreiben müssen..")
          ),
 tabPanel(title = "Grafiken",
          sidebarLayout(
            sidebarPanel(
+             h4("Parameter"),
              sliderInput(inputId = "n_total",
                           label = "Gesamtanzahl an Schülern",
                           value = 240,
@@ -20,31 +25,39 @@ tabPanel(title = "Grafiken",
                           max = 1000,
                           step = 10),
              
+             hr(),
+             
              sliderInput(inputId = "n_klassen",
                           label = "Anzahl Klassen",
                           value = 8,
                           min = 2,
                           max = 20),
              
+             hr(),
+             
              sliderInput(inputId = "int_sd",
                          label = "Standardabweichung des Intercepts",
                          min = 0,
                          max = 30,
                          value = 10),
+             
+             hr(), 
+             
              sliderInput(inputId = "slope_sd",
                          label = "Standardabweichung des Slopes",
                          min = 0, 
                          max = 5,
-                         value = 0),
+                         value = 0,
+                         step = 0.1),
              checkboxInput(inputId = "corr_slope",
                            label = "Korrelierter Slope",
                            value = FALSE)
-
-             
            ),
            mainPanel(
              plotOutput(outputId = "multiplot"),
-             verbatimTextOutput(outputId = "summary", placeholder = TRUE)
+             h4("Summary Output"),
+             verbatimTextOutput(outputId = "summary", placeholder = TRUE),
+             verbatimTextOutput(outputId = "test", placeholder = TRUE)
            )
            )
          )
@@ -52,38 +65,15 @@ tabPanel(title = "Grafiken",
 
 server <- function(input, output) {
   
-  # Funktion zur generierung der Daten
-  ran_inter <- function(n = 240, nklassen = 8, sd_intercept = 10, sd_slope = 0){
-    stunden <- round(runif(n, 1, 30), digits = 0)
-    klasse <- sample(1:nklassen, n, replace = TRUE)
-    effekt_int <- round(rnorm(nklassen, 0, sd_intercept), digits = 1)
-    # effekt_int <- sort(effekt_int)
-    effekt_slope <- round(rnorm(nklassen, 0, sd_slope), digits = 1)
-    # effekt_slope <- sort(effekt_slope)
-    
-    random_intercept <- numeric(n)
-    for (i in 1:n){
-      random_intercept[i] <- effekt_int[klasse[i]]
+  # Laden der richtigen Funktion
+  observeEvent(input$corr_slope, {
+    if (input$corr_slope == FALSE){
+      source("uncorr_ml.R")
+    } else {
+      source("corr_ml.R")
     }
-    
-    random_slope <- numeric(n)
-    for (i in 1:n){
-      random_slope[i] <- effekt_slope[klasse[i]]
-    }
-    
-    error <- round(rnorm(n, 0, 5), digits = 1)
-    leistung <- round(20 + 2.5 * stunden + random_intercept + random_slope * stunden + error, digits = 0)
-    # leistung <- ifelse(leistung > 100, 100, leistung)
-    # leistung <- ifelse(leistung < 0, 0, leistung)
-    # Als Fussnote erwähnen im Beispiel
-    
-    klasse <- as.factor(klasse)
-    levels(klasse) <- paste(1:nklassen, "md", sep = "")
-    
-    ranint_data <- data.frame(stunden, klasse, leistung)
-    return(ranint_data)
-  }
-  
+  })
+
   data_model <- reactive(ran_inter(n = input$n_total, nklassen = input$n_klassen, sd_intercept = input$int_sd, sd_slope = input$slope_sd))
   
   # Model und Koeffizienten berechnen
