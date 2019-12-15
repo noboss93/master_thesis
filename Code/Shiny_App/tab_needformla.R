eaecabd7ab2c54c46bc4625421d17c19ed467768
Noah Bosshart <noahbosshart@gmail.com>
2019-12-15 12:38:39 +0100

    adding new code

--- Code/Shiny_App/tab_needformla.R
+++ Code/Shiny_App/tab_needformla.R
@@ -1,5 +1,15 @@
 fluidRow(
-  box(width = 12,
+  box(width = 6,
     withMathJax(includeMarkdown("tab_needformla.Rmd"))
+  ),
+  tabBox(width = 12,
+
+         tabPanel(title = "R Code",
+                  print("lmer(leistung ~ 1 + (1 | klassen), data = data_model()")
+         ),
+         tabPanel(title = "R Output",
+                     "summary(data_model())"
+         )
   )
+         
 )
