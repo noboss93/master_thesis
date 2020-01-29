606f2ccad30652ec965988a891a8fcd209f360e9
Noah Bosshart <noahbosshart@gmail.com>
2020-01-29 11:35:41 +0100

    improving dgp

--- Code/Shiny_App/dgp_multi_ml.R
+++ Code/Shiny_App/dgp_multi_ml.R
@@ -1,16 +1,26 @@
-gen_ml_data <- function(n = 15000, nklassen = 300, sd_intercept = 2, sd_slope = 2, 
-                        corr = 0, sd_error = 5, y00 = 15, y10 = 0.35){
+gen_ml_data <- function(nschueler = 50, 
+                        nklassen = 300, 
+                        sd_intercept = 2, 
+                        sd_slope = 0, 
+                        corr = 0, 
+                        sd_error = 5,
+                        sd_didaktik = 5,
+                        sd_uebung = 5,
+                        y00 = 15, 
+                        y10 = 0.5,
+                        y01 = 0.5){
   
-  # Creating Treatment as Level-1 Variable
-  uebung <- sample(rep(c(0:29), each = n/length(c(0:29))), n)
-  klasse <- rep(1:nklassen, each = n/nklassen)
+  # Function for truncated normal distribution
+  rtnorm <- function(n, mean, sd, a = -Inf, b = Inf){
+    qnorm(runif(n, pnorm(a, mean, sd), pnorm(b, mean, sd)), mean, sd)
+  }
   
-  # Creating Treatment as Level-2 Variable
-  # anz_math_lektionen <- sample(c(0:6), n, replace = TRUE)
-  # math_lektionen <- c()
-  # for (i in 1:n){
-  #   math_lektionen[i] <- anz_math_lektionen[klasse[i]]
-  # }
+  # Creating Variables
+  n <- nschueler * nklassen
+  error <- rnorm(n, mean = 0, sd = sd_error)
+  uebung <- round(rtnorm(n, mean = 15, sd = sd_uebung, a = 0, b = 30))
+  didaktik <- rep(rnorm(nklassen, mean = 0, sd = sd_didaktik), each = nschueler)
+  klasse <- rep(1:nklassen, each = nschueler)
   
   # Creating random effects of klassen
   covar01 <- corr * sqrt(sd_intercept^2 * sd_slope^2)
@@ -24,36 +34,23 @@ gen_ml_data <- function(n = 15000, nklassen = 300, sd_intercept = 2, sd_slope = 
   effekte <- mvrnorm(n = nklassen, mu = c(0,0), 
                      Sigma = effekte_cov_matrix, empirical = TRUE)
   
-  effekt_int <- effekte[,1]
-  effekt_slope <- effekte[,2]
-  
-  # Creating random effects for Intercept and Slope based on klassen
-  random_intercept <- numeric(n)
-  for (i in 1:n){
-    random_intercept[i] <- effekt_int[klasse[i]]
-  }
-  
-  random_slope <- numeric(n)
-  for (i in 1:n){
-    random_slope[i] <- effekt_slope[klasse[i]]
-  }
-  
-  # Creating random error for individuals
-  error <- rnorm(n, mean = 0, sd = sd_error)
+  random_intercept <- rep(effekte[,1], each = nschueler)
+  random_slope <- rep(effekte[,2], each = nschueler)
   
   # Calculating individual leistung score
-  leistung <- y00 +  
-    y10 * uebung + 
-    random_intercept + 
-    random_slope * uebung + 
-    error
-  
+  leistung <- numeric(n)
+  for (i in 1:n){
+  leistung[i] <- y00 +  
+    y10 * uebung[i] + 
+    y01 * didaktik[i] +
+    random_intercept[i] + 
+    random_slope[i] * uebung[i] + 
+    error[i]
+  }
   # Creating dataframe
   klasse <- as.factor(klasse)
-  levels(klasse) <- paste("Kl", 1:nklassen, sep = ".")
   
-  ml_data <- data.frame(klasse, uebung, leistung, random_intercept, random_slope)
+  ml_data <- data.frame(klasse, uebung, didaktik, leistung, random_intercept, random_slope)
   
   return(ml_data)
 }
-
