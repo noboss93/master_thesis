78f93da4700c2fd75ac56cf10afa0ef3a8b15e0e
Noah Bosshart <noahbosshart@gmail.com>
2020-02-11 12:38:47 +0100

    seeing finish line for applied part

--- Versionen/theorie_analyse.R
+++ Versionen/theorie_analyse.R
@@ -60,8 +60,8 @@ gen_ml_data <- function(n = 15000, nklassen = 300, sd_intercept = 2, sd_slope = 
   # Creating random error for individuals
   error <- rnorm(n, mean = 0, sd = sd_error)
   
-  # Calculating individual leistung score
-  leistung <- b00 +  
+  # Calculating individual punktzahl score
+  punktzahl <- b00 +  
     b10 * uebung + 
     random_intercept + 
     random_slope * uebung + 
@@ -71,7 +71,7 @@ gen_ml_data <- function(n = 15000, nklassen = 300, sd_intercept = 2, sd_slope = 
   klasse <- as.factor(klasse)
   levels(klasse) <- c(1:nklassen)
   
-  ml_data <- data.frame(klasse, uebung, leistung, geschl, didaktik, ses, iq)
+  ml_data <- data.frame(klasse, uebung, punktzahl, geschl, didaktik, ses, iq)
   
   return(ml_data)
 }
@@ -101,17 +101,17 @@ colnames(test) <- c("klasse", "uebung", "punktzahl", "geschl", "fenster", "ses",
 
 
 # Aggregated Dataframe
-leistung_aggr <- c()
+punktzahl_aggr <- c()
 uebung_aggr <- c()
 klasse <- c(1:5)
 
 for (i in 1:5){
-  leistung_aggr[i] <- mean(test[test[,1]==i,3])
+  punktzahl_aggr[i] <- mean(test[test[,1]==i,3])
   uebung_aggr[i] <- mean(test[test[,1]==i,2])
 }
 
-data_aggr <- data.frame(matrix(c(klasse, uebung_aggr, leistung_aggr), ncol = 3, nrow = 5))
-colnames(data_aggr) <- c("klasse", "uebung", "leistung")
+data_aggr <- data.frame(matrix(c(klasse, uebung_aggr, punktzahl_aggr), ncol = 3, nrow = 5))
+colnames(data_aggr) <- c("klasse", "uebung", "punktzahl")
 data_aggr[,1] <- as.factor(data_aggr[,1])
 # Latex Tabelle -----------------------------------------------------------
 
@@ -126,17 +126,17 @@ xtable(data_aggr, digits = 1)
 
 # Regressionsmodelle ------------------------------------------------------
 
-lm0 <- lmer(leistung ~ (1|klasse), data = test)
-lm01 <- lmer(leistung ~ (uebung|klasse), data = test)
-lm2<- lmer(leistung ~ uebung + (1|klasse), data = test)
-lm3 <- lmer(leistung ~ uebung + (uebung | klasse), data = test)
+lm0 <- lmer(punktzahl ~ (1|klasse), data = test)
+lm01 <- lmer(punktzahl ~ (uebung|klasse), data = test)
+lm2<- lmer(punktzahl ~ uebung + (1|klasse), data = test)
+lm3 <- lmer(punktzahl ~ uebung + (uebung | klasse), data = test)
 
-as0 <- lm(data = test, leistung ~ 1)
-as1 <- lm(data = test, leistung ~ math_lektionen)
-as2 <- lm(data = test, leistung ~ math_lektionen + klasse)
-as3 <- lm(data = test, leistung ~ math_lektionen * klasse)
+as0 <- lm(data = test, punktzahl ~ 1)
+as1 <- lm(data = test, punktzahl ~ math_lektionen)
+as2 <- lm(data = test, punktzahl ~ math_lektionen + klasse)
+as3 <- lm(data = test, punktzahl ~ math_lektionen * klasse)
 
-lm_aggr <- lm(data = data_aggr, leistung ~ uebung)
+lm_aggr <- lm(data = data_aggr, punktzahl ~ uebung)
 
 summary(lm0)
 summary(lm2)
@@ -147,21 +147,21 @@ summary(as0)
 # Theory Plots LM ---------------------------------------------------------
 
 # Regression Aggregation
-ggplot(data = data_aggr, mapping = aes(x = uebung, y = leistung))+
+ggplot(data = data_aggr, mapping = aes(x = uebung, y = punktzahl))+
   geom_point(size = 2)+
   geom_smooth(method = "lm", se = FALSE, col = "red", size = 1) +
   labs(x = "Anzahl gelöster Übungsaufgaben", y = "Punktzahl") +
   theme_gray(base_size = 15)
 
 # Regression Disagregation
-dis_lm <- lm(data = test, leistung ~ uebung)
-a <- ggplot(data = test, mapping = aes(x = uebung, y = leistung))+
+dis_lm <- lm(data = test, punktzahl ~ uebung)
+a <- ggplot(data = test, mapping = aes(x = uebung, y = punktzahl))+
   geom_point() +
   geom_smooth(method = "lm", se = FALSE, col = "red", fullrange = TRUE)+
   labs(x = "Anzahl gelöster Übungsaufgaben", y = "Punktzahl", title = "Modell für gesamten Datensatz") +
   theme_gray(base_size = 15)
 
-b <- ggplot(data = test, mapping = aes(x = uebung, y = leistung))+
+b <- ggplot(data = test, mapping = aes(x = uebung, y = punktzahl))+
   geom_point()+
   geom_abline(intercept = coef(dis_lm)[1], slope = coef(dis_lm)[2], size = 1, col = "red")+
   facet_wrap(~klasse)+
@@ -174,14 +174,14 @@ grid.arrange(a,b, nrow = 1)
 
 # Theory Plots HLM --------------------------------------------------------
 
-lm2<- lmer(leistung ~ uebung + (1|klasse), data = test)
+lm2<- lmer(punktzahl ~ uebung + (1|klasse), data = test)
 intercept <- as.numeric(ranef(lm2)$klasse[,1]) + as.numeric(fixef(lm2)[1])
 fix_slope <- rep(as.numeric(fixef(lm2)[2]), times = 5)
 klasse <- c(1:5)
 coef_data <- data.frame(intercept, fix_slope, klasse)
 colors <- c("Gesamtgerade" = "red", "Klassengerade" = "black")
 
-a <- ggplot(data = test, mapping = aes(x = uebung, y = leistung)) + 
+a <- ggplot(data = test, mapping = aes(x = uebung, y = punktzahl)) + 
   geom_point(size = 2) +
   geom_abline(slope = fix_slope, intercept = intercept, size = 1) +
   geom_abline(slope = fix_slope, 
@@ -189,7 +189,7 @@ a <- ggplot(data = test, mapping = aes(x = uebung, y = leistung)) +
   theme_gray(base_size = 15) +
   labs(x = "Anzahl gelöster Übungsaufgaben", y = "Punktzahl", title = "Modell für gesamten Datensatz")
 
-b <- ggplot(data = test, mapping = aes(x = uebung, y = leistung))+
+b <- ggplot(data = test, mapping = aes(x = uebung, y = punktzahl))+
   geom_point()+
   geom_abline(data = coef_data, aes(intercept = intercept, slope = fix_slope, group = klasse, color = "Klassengerade"), size = 1)+
   geom_abline(data = coef_data, aes(intercept = mean(intercept), slope = mean(fix_slope), color = "Gesamtgerade"), size = 1)+
@@ -221,14 +221,14 @@ d <- ggplot(lm2, aes(.fitted, .resid))+
 grid.arrange(c,d, nrow = 1)
 
 
-lm3<- lmer(leistung ~ uebung + (uebung|klasse), data = test)
+lm3<- lmer(punktzahl ~ uebung + (uebung||klasse), data = test)
 intercept <- as.numeric(ranef(lm3)$klasse[,1]) + as.numeric(fixef(lm3)[1])
 slope <- as.numeric(ranef(lm3)$klasse[,2]) + as.numeric(fixef(lm3)[2])
 klasse <- c(1:5)
 coef_data <- data.frame(intercept, slope, klasse)
 colors <- c("Gesamtgerade" = "red", "Klassengerade" = "black")
 
-e <- ggplot(data = test, mapping = aes(x = uebung, y = leistung)) + 
+e <- ggplot(data = test, mapping = aes(x = uebung, y = punktzahl)) + 
   geom_point(size = 2) +
   geom_abline(slope = slope, intercept = intercept, size = 1) +
   geom_abline(slope = mean(slope), 
@@ -236,7 +236,7 @@ e <- ggplot(data = test, mapping = aes(x = uebung, y = leistung)) +
   theme_gray(base_size = 15) +
   labs(x = "Anzahl gelöster Übungsaufgaben", y = "Punktzahl", title = "Modell für gesamten Datensatz")
 
-f <- ggplot(data = test, mapping = aes(x = uebung, y = leistung))+
+f <- ggplot(data = test, mapping = aes(x = uebung, y = punktzahl))+
   geom_point()+
   geom_abline(data = coef_data, aes(intercept = intercept, slope = slope, group = klasse, color = "Klassengerade"), size = 1)+
   geom_abline(data = coef_data, aes(intercept = mean(intercept), slope = mean(slope), color = "Gesamtgerade"), size = 1)+
@@ -258,14 +258,14 @@ ggplot(lm3, aes(.fitted, .resid))+
 
 
 
-lm_neg<- lmer(leistung ~ uebung + (uebung|klasse), data = test_neg)
+lm_neg<- lmer(punktzahl ~ uebung + (uebung|klasse), data = test_neg)
 intercept <- as.numeric(ranef(lm_neg)$klasse[,1]) + as.numeric(fixef(lm_neg)[1])
 slope <- as.numeric(ranef(lm_neg)$klasse[,2]) + as.numeric(fixef(lm_neg)[2])
 klasse <- c(1:5)
 coef_data <- data.frame(intercept, slope, klasse)
 colors <- c("Gesamtgerade" = "red", "Klassengerade" = "black")
 
-neg <- ggplot(data = test_neg, mapping = aes(x = uebung, y = leistung)) + 
+neg <- ggplot(data = test_neg, mapping = aes(x = uebung, y = punktzahl)) + 
   geom_point(size = 2) +
   geom_abline(slope = slope, intercept = intercept, size = 1) +
   geom_abline(slope = mean(slope), 
@@ -274,14 +274,14 @@ neg <- ggplot(data = test_neg, mapping = aes(x = uebung, y = leistung)) +
   labs(x = "Anzahl gelöster Übungsaufgaben", y = "Punktzahl", title = "Negative Korrelation")
 
 
-lm_neutr<- lmer(leistung ~ uebung + (uebung||klasse), data = test_neutr)
+lm_neutr<- lmer(punktzahl ~ uebung + (uebung||klasse), data = test_neutr)
 intercept <- as.numeric(ranef(lm_neutr)$klasse[,1]) + as.numeric(fixef(lm_neutr)[1])
 slope <- as.numeric(ranef(lm_neutr)$klasse[,2]) + as.numeric(fixef(lm_neutr)[2])
 klasse <- c(1:5)
 coef_data <- data.frame(intercept, slope, klasse)
 colors <- c("Gesamtgerade" = "red", "Klassengerade" = "black")
 
-neutr <- ggplot(data = test_neutr, mapping = aes(x = uebung, y = leistung)) + 
+neutr <- ggplot(data = test_neutr, mapping = aes(x = uebung, y = punktzahl)) + 
   geom_point(size = 2) +
   geom_abline(slope = slope, intercept = intercept, size = 1) +
   geom_abline(slope = mean(slope), 
